c94bc7a1fc4c3de88e155ce6146d0cc1daf69e2b
kiamco <kiamcoo@gmail.com>
2019-05-24 09:11:47 -0700

    solved stretch problem with one line

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,61 +26,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-    // getLength passes the length of the array into the callback.
-    return cb(arr.length);
+  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 getLength(items, (len) => {
-    console.log("This is the length of items array: " + len);
+  console.log("This is the length of items array: " + len);
 });
 
 // ----------------------------------------------------------
 function last(arr, cb) {
-    // last passes the last item of the array into the callback.
-    return cb(arr[arr.length - 1]);
+  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
 last(items, (el) => {
-    console.log("This is the last item of the array: " + el);
+  console.log("This is the last item of the array: " + el);
 });
 
 // -----------------------------------------------------------
 function sumNums(x, y, cb) {
-    // sumNums adds two numbers (x, y) and passes the result to the callback.
-    return cb(x + y);
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 sumNums(1, 2, (sum) => {
-    console.log("this is the sum of 1 and 2: " + sum)
+  console.log("this is the sum of 1 and 2: " + sum)
 });
 
 // -----------------------------------------------------------
 function multiplyNums(x, y, cb) {
-    // multiplyNums multiplies two numbers and passes the result to the callback.
-    return cb(x * y);
+  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
 multiplyNums(2, 4, (num) => {
-    console.log("This is the product of 2 and 4: " + num);
+  console.log("This is the product of 2 and 4: " + num);
 });
 
 // -----------------------------------------------------------
 function contains(item, list, cb) {
-    // contains checks if an item is present inside of the given array/list.
-    // Pass true to the callback if it is, otherwise pass false.
-    let isTrue = list.find((fItem) => {
-        return fItem === item
-    });
-    cb(isTrue);
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+  let isTrue = list.find((fItem) => {
+    return fItem === item
+  });
+  cb(isTrue);
 }
 
 
 contains("Gumasdsad", items, (el) => {
-    if (el === true) {
-        console.log("Item found: true");
-    } else {
-        console.log("Item found: false");
-    }
+  if (el === true) {
+    console.log("Item found: true");
+  } else {
+    console.log("Item found: false");
+  }
 });
 
 
@@ -90,9 +90,19 @@ contains("Gumasdsad", items, (el) => {
 
 /* STRETCH PROBLEM */
 
+
+
 function removeDuplicates(array, cb) {
-    // removeDuplicates removes all duplicate values from the given array.
-    // Pass the duplicate free array to the callback function.
-    // Do not mutate the original array.
-    // console.log(array.map());
+  // removeDuplicates removes all duplicate values from the given array.
+  // Pass the duplicate free array to the callback function.
+  // Do not mutate the original array.
+  // cb(array.filter((el, index) => {return array.indexOf(el) >= index })); 
+  return cb(array);
 }
+
+const duplicates = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
+const dedupedArr = removeDuplicates(duplicates, array => array.filter((el, index) => array.indexOf(el) >= index));
+
+console.log("This is the output of the stratch problem: ")
+console.log(dedupedArr);
